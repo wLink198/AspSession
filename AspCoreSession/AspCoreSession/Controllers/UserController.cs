22474550b54bcb46f7ee1453497212557973755f
wLink69 <linhbqd00500@fpt.edu.vn>
2018-10-16 23:16:55 +0700

    gg

---
+++ AspCoreSession/AspCoreSession/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using AspCoreSession.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AspCoreSession.Controllers
+{
+    public class UserController : Controller
+    {
+        public const string SessionKeyName = "_Name";
+ 
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            HttpContext.Session.SetString(SessionKeyName, JsonConvert.SerializeObject(user));
+            return RedirectToAction("Info");
+        }
+
+        public IActionResult Info()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(SessionKeyName)))
+            {
+                return Redirect("~/User/Login");
+            }
+            User user = JsonConvert.DeserializeObject<User>(HttpContext.Session.GetString(SessionKeyName));
+            return View(user);
+        }
+    }
+}
---
+++ AspCoreSession/AspCoreSession/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreSession.Models
+{
+    public class User
+    {
+        private int id;
+        private string username;
+        private string password;
+
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+        public int Id { get => id; set => id = value; }
+    }
+}
